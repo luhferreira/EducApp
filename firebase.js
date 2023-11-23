dd80e7803a85d4ea1cf42a601f6b2dad11f4c117
Luana Ferreira <56826627+luhferreira@users.noreply.github.com>
2023-11-23 14:50:28 -0300

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAPavh-Zx8JMp_SX_b4YZ51gCfGctpEJQg",
+  authDomain: "eduapp-a8b4c.firebaseapp.com",
+  projectId: "eduapp-a8b4c",
+  storageBucket: "eduapp-a8b4c.appspot.com",
+  messagingSenderId: "36506251061",
+  appId: "1:36506251061:web:dcf3ae63546661db6ae67c"
+};
